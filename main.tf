42df28a81e331422a82f3a5e8e9c1812863a3d8c
Eric Dahl <eric.c.dahl@gmail.com>
2017-10-08 13:09:17 -0700

    remove unused instance

--- main.tf
+++ main.tf
@@ -5,16 +5,3 @@ provider "aws" {
 variable "name" {
   default = "tf-ecs-placement"
 }
-
-resource "aws_instance" "default" {
-  instance_type = "t2.micro"
-  ami = "ami-a4c7edb2"
-  vpc_security_group_ids = [
-    "${aws_security_group.default.id}"]
-
-
-  subnet_id = "${aws_subnet.subnet1.id}"
-
-  key_name = "${var.key}"
-}
-
